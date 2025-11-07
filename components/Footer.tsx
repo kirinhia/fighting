5da1f25d70c604509629fe3787f68c4d880c5203
kirinhia <74898074+kirinhia@users.noreply.github.com>
2025-11-07 11:45:23 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import * as React from 'react'
 
-import * as config from '@/lib/site-config'
+import * as config from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
